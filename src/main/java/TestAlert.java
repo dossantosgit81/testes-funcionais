962921064e1a1ce9dd087f38759da2339d8146a2
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2021-11-04 11:46:43 -0300

    Alert confirm

--- src/main/java/TestAlert.java
+++ src/main/java/TestAlert.java
@@ -20,4 +20,44 @@ public class TestAlert {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(textoAlert);
 	}
 	
+	@Test
+	public void deveInteragirComAlertConfirm() {
+		WebDriver driver  = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("confirm")).click();
+		Alert alert = driver.switchTo().alert();
+		String textoAlert = alert.getText();
+		Assert.assertEquals("Confirm Simples", textoAlert);
+		alert.accept();
+		textoAlert = alert.getText();
+		Assert.assertEquals("Confirmado", textoAlert);
+		alert.accept();
+		
+		driver.findElement(By.id("confirm")).click();
+		 alert = driver.switchTo().alert();
+		 textoAlert = alert.getText();
+		Assert.assertEquals("Confirm Simples", textoAlert);
+		alert.dismiss();
+		textoAlert = alert.getText();
+		Assert.assertEquals("Negado", textoAlert);
+		alert.dismiss();
+		
+	}
+	
+	@Test
+	public void deveInteragirComAlertPrompt() {
+		WebDriver driver  = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("prompt")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", alerta.getText());
+		alerta.sendKeys("12");
+		alerta.accept();
+		Assert.assertEquals("Era 12?", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals(":D", alerta.getText());
+		alerta.accept();
+	}
+	
 }
